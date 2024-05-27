da415d57136bcc054de924d7d59f01f4c0d88839
UlkaPulka20 <91197124+UlkaPulka20@users.noreply.github.com>
2024-05-27 11:14:22 +0300

    Update main.py

--- lab10/main.py
+++ lab10/main.py
@@ -4,9 +4,25 @@ import csv
 from PIL import Image, ImageFilter
 
 
-# For lab-9-2
+# 2
+VALID_IMAGE = ("jpg", "jpeg", "png")
+#1
+img = Image.open('cats/Cat.jpg')
 
+img = img.filter(ImageFilter.CONTOUR)
+img.save("1.jpg")
 
+img = img.filter(ImageFilter.DETAIL)
+img.save("2.jpg")
+
+img = img.filter(ImageFilter.EDGE_ENHANCE)
+img.save("3.jpg")
+
+img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+img.save("4.jpg")
+
+img = img.filter(ImageFilter.EMBOSS)
+img.save("5.jpg")
 
 def lab_9():
 
